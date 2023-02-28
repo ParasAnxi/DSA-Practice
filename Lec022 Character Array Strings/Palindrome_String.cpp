b9ed0b7cf51c58e760d2256215a925ba870b998a
ParasAnxi <paras.bhandari1010@gmail.com>
2023-02-28 10:53:36 +0530

    p

--- Lec022 Character Array Strings/Palindrome_String.cpp
+++ Lec022 Character Array Strings/Palindrome_String.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+char toLowerCase(char ch){
+    if(ch>='a' && ch<='z'){
+        return ch;
+    }
+    else{
+        char low = ch - 'A' + 'a';
+        return low;
+    }
+}
+char toUpperCase(char ch){
+    if(ch>='A' && ch<='Z'){
+        return ch;
+    }
+    else{
+        char low = ch - 'a' + 'A';
+        return low;
+    }
+}
+int toNum(char ch){
+    if(ch>=0 && ch<=9){
+        ch - '0';
+    }
+        return ch;
+}
 int getLength(char name[]){
     int count = 0;
     for(int i = 0; name[i]!='\0';i++){
@@ -32,5 +55,9 @@ int main(){
     else{
         cout<<"Not plaindrome"<<endl;
     }
+
+    cout<<"CHARACTER IS "<<toLowerCase('P')<<endl;;
+    cout<<"character is "<<toUpperCase('a')<<endl;;
+    cout<<"To num is "<<toNum('9');
 
 }
