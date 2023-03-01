6567d1090862ce7aa037ed1d496c1d349eed15bd
ParasAnxi <paras.bhandari1010@gmail.com>
2023-03-01 11:05:23 +0530

    row sum

--- Lec023 2D Array/Row_Wise_Sum.cpp
+++ Lec023 2D Array/Row_Wise_Sum.cpp
@@ -1,8 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int (){
-
+void rowSum(int arr[][4],int i ,int j){
+     for(int i = 0 ; i<3 ; i++){
+        int sum = 0;
+        for(int j = 0; j<4 ;j++){
+            sum+=arr[i][j];
+        }
+        cout<<sum<<" ";
+     }
 }
 int main(){
     int arr[3][4];
@@ -12,5 +18,5 @@ int main(){
             cin>>arr[i][j];
         }
     }
-
+    rowSum(arr,3,4);
 }
