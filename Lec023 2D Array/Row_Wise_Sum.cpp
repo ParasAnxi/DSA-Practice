e9b9644309f643a53625843a602531e084ab3f2e
ParasAnxi <paras.bhandari1010@gmail.com>
2023-03-01 11:00:59 +0530

    lS

---
+++ Lec023 2D Array/Row_Wise_Sum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int (){
+
+}
+int main(){
+    int arr[3][4];
+    cout<<"Enter the Elements : ";
+    for(int i = 0 ; i<3 ; i++){
+        for(int j = 0; j<4 ;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+}
