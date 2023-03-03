d9c948111f93842f67e1ee8067b016c75ea72ab5
ParasAnxi <paras.bhandari1010@gmail.com>
2023-03-03 15:27:55 +0530

    Reverse LL

---
+++ Lec045 Linked List/Reverse_A_Linked_List.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node* reverseLinkedList(Node *head){
+    Node* prev = NULL;
+    Node* forward = NULL;
+    Node* curr = head;
+
+    while(curr!=NULL){
+        forward=curr->next;
+        curr->next = prev;
+        prev=curr;
+        curr=forward;
+    }
+}
