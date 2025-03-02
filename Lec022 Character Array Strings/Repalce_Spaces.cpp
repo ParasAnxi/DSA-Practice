7e02e329351812aa3701cda5e3c9d5932c86b213
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-03-02 14:12:28 +0530

    replace str

--- Lec022 Character Array Strings/Repalce_Spaces.cpp
+++ Lec022 Character Array Strings/Repalce_Spaces.cpp
@@ -1,20 +1,52 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
-string replaceSpaces(string &str){
+string replaceSpaces(string &str)
+{
 	string temp = "";
-	for(int i = 0 ;i<str.length();i++){
-		if(str[i]==' '){
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')
+		{
 			temp.push_back('@');
 			temp.push_back('4');
 			temp.push_back('0');
 		}
-		else{
+		else
+		{
 			temp.push_back(str[i]);
 		}
 	}
-		return temp;
+	return temp;
 }
-int main(){
-    string s = "This is a great way to do thing.";
-    cout<<replaceSpaces(s);
+string replaceSpaces1(string &str)
+{
+	int len = str.length();
+	int spaces = 0;
+	for(int i = 0 ; i < len;i++){
+		if(str[i] == ' '){
+			spaces++;
+		}
+	}
+	int newLen = len + spaces * 2;
+	str.resize(newLen);
+	int i = len - 1; 
+	int j = newLen - 1;
+	while(i>=0){
+		if(str[i] == ' '){
+			str[j] = '0';
+			str[j - 1] = '4';
+			str[j - 2] = '@';
+			j-=3;
+		}else{
+			str[j] = str[i];
+			j--;
+		}
+		i--;
+	}
+	return str;
+}
+int main()
+{
+	string s = "This is a great way to do thing.";
+	cout << replaceSpaces1(s);
 }
